bcfc7ac75d8ddf8dd495033a45f27a76806dc310
alessandroargentieri <alessandro@civo.com>
2024-12-23 12:12:10 +0100

    fix logic http

--- http/server/hello_world/main.go
+++ http/server/hello_world/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-  "net/http"
-  "log"
+	"log"
+	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-  queryString := r.URL.Query()
-  name, ok := queryString["name"]
-  if !ok {
-          w.WriteHeader(400)
-          w.Write([]byte("Missing name"))
-          return
-      }
-  msg := "Hello " + name[0]
-  w.Write([]byte(msg))
+	queryString := r.URL.Query()
+	name, ok := queryString["name"]
+	if !ok {
+		w.WriteHeader(400)
+		w.Write([]byte("Missing name"))
+		return
+	}
+	msg := "Hello " + name[0]
+	w.Write([]byte(msg))
 }
 
 func hiHandler(w http.ResponseWriter, r *http.Request) {
-   http.ServeFile(w, r, "./index.html")
+	http.ServeFile(w, r, "./index.html")
 }
 
 func main() {
-  http.HandleFunc("/hello", helloHandler)
-  http.HandleFunc("/hi", hiHandler)            
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/hi", hiHandler)
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+
 }
